204b5d9876b438bf9f4ae53212f980ddb335ea1f
s-hir <none>
2016-10-03 12:54:23 -0400

    Qt 5.7 changes

--- src/ui/aboutdialog.cpp
+++ src/ui/aboutdialog.cpp
@@ -7,7 +7,7 @@ AboutDialog::AboutDialog(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QString text = ui->label_info->text().arg(QString::fromAscii(VERSION)).arg(QString::fromAscii(__DATE__));
+    QString text = ui->label_info->text().arg(QString::fromStdString(VERSION)).arg(QString::fromStdString(__DATE__));
     ui->label_info->setText(text);
 }
 
